ab1d52b7e9a344832deecc83652e1f24326ec1ee
Rudy <rudysilva22@gmail.com>
2019-12-09 20:25:38 +1100

    new files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BidderRegistrationComponent } from './bidder/bidder-registration/bidder-registration.component';
+import { BidderNumberComponent } from './bidder/bidder-number/bidder-number.component';
+import { BidderUsernameComponent } from './bidder/bidder-username/bidder-username.component';
+import { BillingHistoryListComponent } from './bidder/billing-history-list/billing-history-list.component';
+import { BillingHistoryListItemComponent } from './bidder/billing-history-list-item/billing-history-list-item.component';
+import { BidderHeaderComponent } from './bidder/bidder-header/bidder-header.component';
+import { HttpClientModule } from '@angular/common/http';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BidderRegistrationComponent,
+    BidderNumberComponent,
+    BidderUsernameComponent,
+    BillingHistoryListComponent,
+    BillingHistoryListItemComponent,
+    BidderHeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
